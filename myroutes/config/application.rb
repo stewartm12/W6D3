e03052d60447d442ad5094c8484d2fe4a88f2b06
stewart morales <stewartmorales129@gmail.com>
2021-02-03 17:53:56 -0500

    endofday

--- myroutes/config/application.rb
+++ myroutes/config/application.rb
@@ -10,7 +10,6 @@ module Myroutes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    # skip_before_action :verify_authenticity_token 
     # skip_before_action :verify_authenticity_token
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
